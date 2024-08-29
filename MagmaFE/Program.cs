f6f84cf3b3c33ce875e1e52775aae76b04d8d2ba
Stepan <stephen.jr@selyuk.com>
2024-08-29 03:26:51 +0300

    Loading bar, get every property from statistics

--- MagmaFE/Program.cs
+++ MagmaFE/Program.cs
@@ -1,6 +1,7 @@
 using Blazored.LocalStorage;
 using RefreshApiClient;
 using MagmaFE;
+using MagmaFE.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -11,6 +12,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(sp => new RefreshClientService(new Uri("https://lbp.littlebigrefresh.com")));
+builder.Services.AddSingleton<LoadingService>();
 builder.Logging.SetMinimumLevel(LogLevel.Debug);
 
 await builder.Build().RunAsync();
---
+++ MagmaFE/Services/LoadingService.cs
@@ -0,0 +1,20 @@
+﻿namespace MagmaFE.Services;
+
+public class LoadingService
+{
+    public event Action OnChange;
+    private bool isLoading;
+
+    public bool IsLoading
+    {
+        get => isLoading;
+        set
+        {
+            if (isLoading != value)
+            {
+                isLoading = value;
+                OnChange?.Invoke();
+            }
+        }
+    }
+}
